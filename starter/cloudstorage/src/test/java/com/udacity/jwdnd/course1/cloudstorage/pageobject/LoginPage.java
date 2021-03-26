132f376315dc18961f2ddb790dd5a18b0b38a43d
Vinny <vinnytorde@gmail.com>
2021-03-26 16:23:03 -0500

    cleanup

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageobject/LoginPage.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageobject/LoginPage.java
@@ -1,7 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.pageobject;
 
 import com.udacity.jwdnd.course1.cloudstorage.testutils.JavascriptEvents;
-import java.awt.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
